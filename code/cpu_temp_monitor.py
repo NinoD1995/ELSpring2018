fced23ce6a1f1b9fe6ba0719884d9e4d35c90f7d
N03097188 <nguyenm1@hawkmail.newpaltz.edu>
2018-02-11 02:42:08 +0000

    crete a python script to monitor cpu temperatur create cron job to execute this cpu temp python script ever minute

--- code/cpu_temp_monitor.py
+++ code/cpu_temp_monitor.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
+
 import os
 import time
+import subprocess
+import commands
 
-#os.system("script")
-while 1:
-     os.system("date")
-     time.sleep(2)
+     ## command to run - return cpu temp(unknown units) ##
+cmd_linux = "cat /sys/class/thermal/thermal_zone0/temp "
+
+     ## run it ##
+output = subprocess.check_output(cmd_linux, shell=True)
+file = open('temps.csv', 'a')
+
+num_lines = sum(1 for line in open('temps.csv'))
+if(num_lines < 1):        
+     file.write("\"Temp\"\n")
+
+file.write(output[:5] + "\n")
+file.close()
