0782e7e18d58b6df71bacac352678e1ec37ad5a5
N03097188 <nguyenm1@hawkmail.newpaltz.edu>
2018-02-11 03:34:28 +0000

    revise temp_monitor python script to print Date, Time, and Temp

--- code/cpu_temp_monitor.py
+++ code/cpu_temp_monitor.py
@@ -7,14 +7,21 @@ import commands
 
      ## command to run - return cpu temp(unknown units) ##
 cmd_linux = "cat /sys/class/thermal/thermal_zone0/temp "
+cmd_linux1 = "date "
+
 
      ## run it ##
 output = subprocess.check_output(cmd_linux, shell=True)
+output1 = subprocess.check_output(cmd_linux1, shell=True)
+
 file = open('temps.csv', 'a')
 
 num_lines = sum(1 for line in open('temps.csv'))
 if(num_lines < 1):        
-     file.write("\"Temp\"\n")
+     file.write("\"Date_Time\",\"Temp\"\n")
 
+file.write(output1[:len(output1)-2] + ",")
 file.write(output[:5] + "\n")
+
+
 file.close()
