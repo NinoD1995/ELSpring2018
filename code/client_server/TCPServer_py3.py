b6592a8a4898cf6842e3dbc379c6738c68204635
N03097188 <nguyenm1@hawkmail.newpaltz.edu>
2018-02-27 17:39:47 -0500

    Add TCP Client/Server python script

---
+++ code/client_server/TCPServer_py3.py
@@ -0,0 +1,16 @@
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(("",serverPort))
+serverSocket.listen(1)
+print ("The TCP server is ready to receive ... ")
+
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    print ("Received From Client: ", sentence)
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    print ("Sent back to Client: ", capitalizedSentence)
+    connectionSocket.close()
+
